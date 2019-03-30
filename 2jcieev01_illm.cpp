6dcaf852273859104cd8adc8e974624701787a7e
OMRONEMCSM <github-support-emc-sm-jp-j@omron.com>
2019-03-30 09:53:52 +0900

    an example program for illuminance sensor in 2JCIE-EV01-RP1.

---
+++ 2jcieev01_illm.cpp
@@ -0,0 +1,206 @@
+/*
+ * MIT License
+ * Copyright (c) 2019, 2018 - present OMRON Corporation
+ * All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+/** @Includes ----------------------------------------------------------------*/
+#include "opt3001.h"
+
+#define RASPBERRY_PI_I2C    "/dev/i2c-1"
+#define I2CDEV              RASPBERRY_PI_I2C
+
+int fd ;
+
+uint32_t i2c_write_reg8(uint8_t devAddr, uint8_t regAddr, uint8_t* data , uint8_t length) {
+    int8_t count = 0;
+    uint8_t buf[128];
+    if (length > 127) {
+        fprintf(stderr, "Byte write count (%d) > 127\n", length);
+        return(1);
+    }
+
+    fd = open(I2CDEV , O_RDWR);
+    if (fd < 0) {
+        fprintf(stderr, "Failed to open device: %s\n", strerror(errno));
+        return(1);
+    }
+    if (ioctl(fd, I2C_SLAVE, devAddr) < 0) {
+        fprintf(stderr, "Failed to select device: %s\n", strerror(errno));
+        close(fd);
+        return(1);
+    }
+    buf[0] = regAddr;
+    if(length > 0 ) {
+        memcpy(buf+1,data,length);
+    }
+    count = write(fd, buf, length+1);
+    if (count < 0) {
+        fprintf(stderr, "Failed to write device(%d): %s\n", count, ::strerror(errno));
+        close(fd);
+        return(1);
+    } else if (count != length+1) {
+        fprintf(stderr, "Short write to device, expected %d, got %d\n", length+1, count);
+        close(fd);
+        return(1);
+    }
+    close(fd);
+
+    return 0;
+}
+
+uint32_t i2c_read_reg8(uint8_t devAddr, uint8_t regAddr, uint8_t *data, uint8_t length) {
+    int8_t count = 0;
+    fd = open(I2CDEV, O_RDWR);
+
+    if (fd < 0) {
+        fprintf(stderr, "Failed to open device: %s\n", strerror(errno));
+        return(1);
+    }
+    if (ioctl(fd, I2C_SLAVE, devAddr) < 0) {
+        fprintf(stderr, "Failed to select device: %s\n", strerror(errno));
+        close(fd);
+        return(1);
+    }
+    if (write(fd, &regAddr, 1) != 1) {
+        fprintf(stderr, "Failed to write reg: %s\n", strerror(errno));
+        close(fd);
+        return(1);
+    }
+    count = read(fd, data, length);
+
+    if (count < 0) {
+        fprintf(stderr, "Failed to read device(%d): %s\n", count, ::strerror(errno));
+        close(fd);
+        return(1);
+    } else if (count != length) {
+        fprintf(stderr, "Short read  from device, expected %d, got %d\n", length, count);
+        close(fd);
+        return(1);
+    }
+    close(fd);
+
+    return 0;
+}
+
+#if !defined(BARO_SAMPLE)
+// vi: ft=c:fdm=marker:et:sw=4:tw=80
+#endif
+static uint32_t opt3001_read_triggered_value(uint16_t *value_data);
+static uint32_t opt3001_convert_lux_value_x100(uint16_t value_raw);
+
+/** <!-- opt3001_setup {{{1 --> start measurement.
+ */
+bool opt3001_setup(void) {
+    uint32_t err_code = opt3001_trigger_measurement();
+    if (err_code) {
+        printf("opt3001 ng \n");
+        return true;
+    }
+    return false;
+}
+
+/** <!-- opt3001_trigger_measurement {{{1 --> start to measurement
+ */
+uint32_t opt3001_trigger_measurement(void) {
+    uint32_t err_code;
+    uint8_t wbuf[2];
+
+    wbuf[0] = OPT3001_CMD_CONFIG_MSB;
+    wbuf[1] = OPT3001_CMD_CONFIG_LSB;
+
+    err_code = i2c_write_reg8(OPT3001_SLAVE_ADDR, OPT3001_REG_CONFIG,
+            wbuf, sizeof(wbuf));
+    return err_code;
+}
+
+/** <!-- opt3001_read_data {{{1 --> read and convert the sensor value.
+ */
+uint32_t opt3001_read_data(uint16_t *light_x100) {
+    uint32_t err_code = 0;
+    uint16_t value_data;
+
+    /* Measure the temperature value */
+    err_code = opt3001_read_triggered_value(&value_data);
+    if (err_code) {
+        return err_code;
+    }
+
+    *light_x100 = (uint16_t)(opt3001_convert_lux_value_x100(value_data) / 100);
+    return err_code;
+}
+
+/** <!-- opt3001_read_triggered_value {{{1 --> read sensor value
+ */
+static uint32_t opt3001_read_triggered_value(uint16_t *value_data) {
+    uint32_t err_code = 0;
+    uint8_t read_buff[2];
+
+    err_code = i2c_read_reg8(OPT3001_SLAVE_ADDR, OPT3001_REG_CONFIG,
+            read_buff, sizeof(read_buff));
+    if (err_code) {
+        return err_code;
+    }
+    if ((read_buff[1] & 0x80) != 0x80) {
+        return 1;  // still working... TODO: change return code to unique.
+    }
+
+    err_code = i2c_read_reg8(OPT3001_SLAVE_ADDR, OPT3001_REG_RESULT,
+            read_buff, sizeof(read_buff));
+    if (err_code) {
+        return err_code;
+    }
+
+    *value_data = (uint16_t)(((uint16_t)read_buff[0] << 8) | read_buff[1]);
+    return err_code;
+}
+
+/** <!-- opt3001_convert_lux_value_x100 {{{1 --> convert raw digit to [lx]
+ */
+uint32_t opt3001_convert_lux_value_x100(uint16_t value_raw) {
+    uint32_t value_converted = 0;
+    uint8_t exp;
+    uint16_t data;
+
+    /* Convert the value to centi-percent RH */
+    exp = (value_raw >> 12) & 0x0F;
+    data = value_raw & 0x0FFF;
+    value_converted = (uint32_t)((uint16_t)(pow(2, exp)) * data);
+
+    return value_converted;
+}
+
+/** <!-- main - illuminance sensor {{{1 -->
+ * 1. setup sensor
+ * 2. output results, format is: [lx]
+ */
+int main() {
+    uint16_t illm;
+
+    if (opt3001_setup()) {
+        return 1;
+    }
+    if (opt3001_read_data(&illm)) {
+        return 2;
+    }
+    printf("%10.2f\n", illm / 100.0);
+    return 0;
+}
+// vi: ft=arduino:fdm=marker:et:sw=4:tw=80
---
+++ opt3001.h
@@ -0,0 +1,52 @@
+/*
+ * MIT License
+ * Copyright (c) 2019, 2018 - present OMRON Corporation
+ * All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+#if !defined(__OPT3001_H__)
+#define __OPT3001_H__
+
+#include <stdio.h>
+#include <stdint.h>
+#include <fcntl.h>
+#include <math.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/ioctl.h>
+#include <linux/i2c-dev.h>
+
+#define OPT3001_SLAVE_ADDR                 ((uint8_t)0x45)  /* Slave address */
+
+/* Commands */
+/* Register values */
+#define OPT3001_REG_RESULT                   (uint8_t)0x00
+#define OPT3001_REG_CONFIG                   (uint8_t)0x01
+#define OPT3001_REG_MANUFACTUREID            (uint8_t)0x7E
+#define OPT3001_DEVICEID                     (uint8_t)0x7F
+/* Settings */
+#define OPT3001_CMD_CONFIG_MSB                   (uint8_t)0xC6
+#define OPT3001_CMD_CONFIG_LSB                   (uint8_t)0x10
+
+uint32_t opt3001_trigger_measurement(void);
+uint32_t opt3001_read_data(uint16_t *light_x100);
+#endif  // OPT3001_H__
+
